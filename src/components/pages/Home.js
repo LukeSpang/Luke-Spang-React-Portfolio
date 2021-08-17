523b0023e191688eed40c2a766ae52fdb01837c3
Luke Spang <lspang20@gmail.com>
2021-08-17 15:14:34 -0400

    got the script tag for md working, added a slide show for projects

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { MDBBtn, MDBContainer } from 'mdb-react-ui-kit';
 import Header from "../Header.js";
-import Navbar from "../Navbar.js";
+//import Navbar from "../Navbar.js";
+
 
 export default function Home() {
   return (
     <MDBContainer fluid>
     <div>
 
-    <Header />
     </div>
     </MDBContainer>
     
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,47 +1,95 @@
 import React from 'react';
 
+
 export default function Projects(){
     return(
-        <section class="projects-section bg-light" id="projects">
-        <div class="container px-4 px-lg-5">
-            <div class="row gx-0 mb-4 mb-lg-5 align-items-center">
-                <div class="col-xl-8 col-lg-7"><a target="_blank" href="https://slown137.github.io/Music-Search-Project/#"><img class="img-fluid mb-3 mb-lg-0" src="assets/img/FinalMusic.png" alt="..." /></a></div>
-                <div class="col-xl-4 col-lg-5">
-                    <div class="featured-text text-center text-lg-left">
-                        <h4>Music Search!</h4>
-                        <p class="text-black-50 mb-0">The Music Search web application uses three powerful API's (genius, lyric, and deezer) to present the user with song lyrics and a sample clip of the same song! Technologies also used in this project are HTML, CSS, JavaScript, and Zurb Foundation. The Live Deployment can be found at <a target="_blank" href="https://slown137.github.io/Music-Search-Project/#">https://slown137.github.io/Music-Search-Project/#</a> </p>
-                    </div>
-                </div>
-            </div>
-            <div class="row gx-0 mb-5 mb-lg-0 justify-content-center">
-                <div class="col-lg-6"><a target="_blank" href="https://map-marauders.herokuapp.com/"><img class="img-fluid" src="assets/img/MapGamePage2.png" alt="Map Game Page" /></a></div>
-                <div class="col-lg-6">
-                    <div class="bg-black text-center h-100 project">
-                        <div class="d-flex h-100">
-                            <div class="project-text w-100 my-auto text-center text-lg-left">
-                                <h4 class="text-white">Map Marauders!</h4>
-                                <p class="mb-0 text-white-50">Map Marauders is a multiplayer geo guessing game! Create your own personal account and play with your friends. Guess the right city in the live chat and you will get a point. Using the google maps API, destinations are limitless! Other technologies used are HTML, CSS, Bootstrap, Handlebars, JavaScript, jQuery, RestAPI, Socket.io, Express, Node, MySQL, Passport, Sequelize, and Bcrypt. The Live Deployment can be found at <a target="_blank" href="https://map-marauders.herokuapp.com/">https://map-marauders.herokuapp.com/</a></p>
-                                <hr class="d-none d-lg-block mb-0 ms-0" />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="row gx-0 justify-content-center">
-                <div class="col-lg-6"><a target="_blank" href="https://lukespang.github.io/Personal-Day-Planner/"><img class="img-fluid" src="assets/img/FinalPlanner.png" alt="..." /></a></div>
-                <div class="col-lg-6 order-lg-first">
-                    <div class="bg-black text-center h-100 project">
-                        <div class="d-flex h-100">
-                            <div class="project-text w-100 my-auto text-center text-lg-right">
-                                <h4 class="text-white">Personal Day Planner</h4>
-                                <p class="mb-0 text-white-50">The Personal Day Planner will help you stay organized during these crazy times we are living through. Using JavaScript and local storage, see what you have added to your to do list, even when you leave the page! Other technologies used were HTML, CSS, and JS Moment. The Live Deployment can be found at <a target="_blank" href="https://lukespang.github.io/Personal-Day-Planner/">https://lukespang.github.io/Personal-Day-Planner/</a> </p>
-                                <hr class="d-none d-lg-block mb-0 me-0" />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+        
+
+<div
+  id="carouselBasicExample"
+  class="carousel slide carousel-fade"
+  data-mdb-ride="carousel"
+>
+  <div class="carousel-indicators">
+    <button
+      type="button"
+      data-mdb-target="#carouselBasicExample"
+      data-mdb-slide-to="0"
+      class="active"
+      aria-current="true"
+      aria-label="Slide 1"
+    ></button>
+    <button
+      type="button"
+      data-mdb-target="#carouselBasicExample"
+      data-mdb-slide-to="1"
+      aria-label="Slide 2"
+    ></button>
+    <button
+      type="button"
+      data-mdb-target="#carouselBasicExample"
+      data-mdb-slide-to="2"
+      aria-label="Slide 3"
+    ></button>
+  </div>
+
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img
+        src="https://mdbootstrap.com/img/Photos/Slides/img%20(15).jpg"
+        class="d-block w-100"
+        alt="..."
+      />
+      <div class="carousel-caption d-none d-md-block">
+        <h5>First slide label</h5>
+        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+      </div>
+    </div>
+
+    <div class="carousel-item">
+      <img
+        src="https://mdbootstrap.com/img/Photos/Slides/img%20(22).jpg"
+        class="d-block w-100"
+        alt="..."
+      />
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Second slide label</h5>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
+    </div>
+
+    <div class="carousel-item">
+      <img
+        src="https://mdbootstrap.com/img/Photos/Slides/img%20(23).jpg"
+        class="d-block w-100"
+        alt="..."
+      />
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Third slide label</h5>
+        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+      </div>
+    </div>
+  </div>
+
+  <button
+    class="carousel-control-prev"
+    type="button"
+    data-mdb-target="#carouselBasicExample"
+    data-mdb-slide="prev"
+  >
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button
+    class="carousel-control-next"
+    type="button"
+    data-mdb-target="#carouselBasicExample"
+    data-mdb-slide="next"
+  >
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
     )
 }
