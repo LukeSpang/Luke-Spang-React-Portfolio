00d207c8823a68919b4a053ef6b22125aef0009e
Luke Spang <lspang20@gmail.com>
2021-08-20 12:46:16 -0400

    added new contact form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,60 +1,32 @@
-import React from 'react';
+import React from "react";
+import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBIcon, MDBInput } from 'mdbreact';
 
-export default function Contact(){
-    return(
-        <section class="signup-section" id="contact">
-            <div class="container px-4 px-lg-5">
-                <div class="row gx-4 gx-lg-5">
-                    <div class="col-md-10 col-lg-8 mx-auto text-center">
-                        <i class="far fa-paper-plane fa-2x mb-2 text-white"></i>
-                        <h2 class="text-white mb-5">Reach out to me!</h2>
-                        <form class="form-signup d-flex flex-column flex-sm-row">
-                            <input class="form-control flex-fill me-0 me-sm-2 mb-3 mb-sm-0" id="inputEmail" type="email" placeholder="Enter email address..." />
-                            <button class="btn btn-primary" type="submit">Send</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </section>,
-        
-        <section class="contact-section bg-black">
-            <div class="container px-4 px-lg-5">
-                <div class="row gx-4 gx-lg-5">
-                    <div class="col-md-4 mb-3 mb-md-0">
-                        <div class="card py-4 h-100">
-                            <div class="card-body text-center">
-                                <i class="fas fa-map-marked-alt text-primary mb-2"></i>
-                                <h4 class="text-uppercase m-0">Based Out Of</h4>
-                                <hr class="my-4 mx-auto" />
-                                <div class="small text-black-50">Raleigh, North Carolina</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-4 mb-3 mb-md-0">
-                        <div class="card py-4 h-100">
-                            <div class="card-body text-center">
-                                <i class="fas fa-envelope text-primary mb-2"></i>
-                                <h4 class="text-uppercase m-0">Email</h4>
-                                <hr class="my-4 mx-auto" />
-                                <div class="small text-black-50"><a href="#!">Lspang20@gmail.com</a></div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-4 mb-3 mb-md-0">
-                        <div class="card py-4 h-100">
-                            <div class="card-body text-center">
-                                <i class="fas fa-mobile-alt text-primary mb-2"></i>
-                                <h4 class="text-uppercase m-0">Phone</h4>
-                                <hr class="my-4 mx-auto" />
-                                <div class="small text-black-50">+1 (717) 341-3763</div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="social d-flex justify-content-center">
-                    <a class="mx-2" target="_blank" href="https://www.linkedin.com/in/luke-spang-a6a960207/"><i class="fab fa-linkedin"></i></a>
-                    <a class="mx-2" target="_blank" href="https://github.com/LukeSpang"><i class="fab fa-github"></i></a>
-                </div>
-            </div>
-        </section>
-    )}
+const Contact = () => {
+return (
+<MDBContainer>
+  <MDBRow>
+    <MDBCol md="6">
+      <form>
+        <p className="h5 text-center mb-4">Write to us</p>
+        <div className="grey-text">
+          <MDBInput label="Your name" icon="user" group type="text" validate error="wrong"
+            success="right" />
+          <MDBInput label="Your email" icon="envelope" group type="email" validate error="wrong"
+            success="right" />
+          <MDBInput label="Subject" icon="tag" group type="text" validate error="wrong" success="right" />
+          <MDBInput type="textarea" rows="2" label="Your message" icon="pencil-alt" />
+        </div>
+        <div className="text-center">
+          <MDBBtn outline color="secondary">
+            Send
+            <MDBIcon far icon="paper-plane" className="ml-1" />
+          </MDBBtn>
+        </div>
+      </form>
+    </MDBCol>
+  </MDBRow>
+</MDBContainer>
+);
+};
+
+export default Contact;
