544f44726f37eb39264e124e920e712f1524e231
Luke Spang <lspang20@gmail.com>
2021-08-20 13:42:44 -0400

    added more to home page, still needs content

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,17 +1,71 @@
 import React from 'react';
-import { MDBBtn, MDBContainer } from 'mdb-react-ui-kit';
-import Header from "../Header.js";
-import Footer from "../Footer.js";
+import { MDBCard, MDBCardTitle, MDBBtn, MDBRow, MDBCol, MDBIcon } from 'mdbreact';
 
-
-export default function Home() {
+const Home = () => {
   return (
-    
-    <div>
-      
+    <MDBRow>
+      <MDBCol md='6'>
+        <MDBCard
+          className='card-image'
+          style={{
+            backgroundImage:
+              "url('https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Work/4-col/img%20%2814%29.jpg')"
+          }}
+        >
+          <div className='text-white text-center d-flex align-items-center rgba-black-strong py-5 px-4'>
+            <div>
+              <h5 className='pink-text'>
+                <MDBIcon icon='desktop' /> Front End
+              </h5>
+              <MDBCardTitle tag='h3' className='pt-2'>
+                <strong>This is card title</strong>
+              </MDBCardTitle>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Repellat fugiat, laboriosam, voluptatem, optio vero odio nam sit
+                officia accusamus minus error nisi architecto nulla ipsum
+                dignissimos. Odit sed qui, dolorum!
+              </p>
+              <MDBBtn  color='pink'>
+                <MDBIcon icon='clone left' /> See More
+                
+              </MDBBtn>
+            </div>
+          </div>
+        </MDBCard>
+      </MDBCol>
 
-    </div>
-   
-    
-  );
+      <MDBCol md='6'>
+        <MDBCard
+          className='card-image'
+          style={{
+            backgroundImage:
+              "url('https://mdbootstrap.com/img/Photos/Horizontal/City/6-col/img%20(47).jpg')"
+          }}
+        >
+          <div className='text-white text-center d-flex align-items-center rgba-indigo-strong py-5 px-4'>
+            <div>
+              <h5 className='orange-text'>
+                <MDBIcon icon='desktop' /> Back End
+              </h5>
+              <MDBCardTitle tag='h3' className='pt-2'>
+                <strong>This is card title</strong>
+              </MDBCardTitle>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Repellat fugiat, laboriosam, voluptatem, optio vero odio nam sit
+                officia accusamus minus error nisi architecto nulla ipsum
+                dignissimos. Odit sed qui, dolorum!
+              </p>
+              <MDBBtn  color='deep-orange'>
+                <MDBIcon icon='clone left' /> See More
+              </MDBBtn>
+            </div>
+          </div>
+        </MDBCard>
+      </MDBCol>
+    </MDBRow>
+  )
 }
+
+export default Home;
